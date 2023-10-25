c75a475614fe28913ab5950483ef50dd1f7de8b3
VChoukwale <choukwale.v@northeastern.edu>
2023-10-24 21:45:04 -0700

    my chnagse

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author abhilashkumargorle
+ */
+public class DatabaseConnection {
+    
+       private static final String url = "jdbc:mysql://localhost:3306/mydb";
+    private static final String username = "root";
+    private static final String password = "root@1234";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
